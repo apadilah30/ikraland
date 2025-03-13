2f50b3b6ac935b75e1b7eb844e828e6ff900142a
Nyanta-23 <muhamadilhan02404@gmail.com>
2025-03-13 13:00:05 +0700

    feat: responsive detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,14 +5,14 @@
 
   <x-back linkTo="/"/>
 
-  <section class="container px-0 overflow-x-hidden">
+  <section class="container">
 
     <div class="row justify-content-center px-0 position-relative">
-      <div id="detail-img" class="col-12 px-0 z-0 height-image">
+      <div id="detail-img" class=" height-image">
         <img src="{{ URL('images/tanaman-1.png') }}" alt="..." class="img-fluid unzoom-image object-fit-cover" />
       </div>
 
-      <div class="col-12 z-1 bg-white-cust pt-3 px-4 mx-2 border rounded-5 rounded-bottom-0">
+      <div class="col-12 z-1 bg-white-cust pt-3 px-4 border rounded-5 rounded-bottom-0">
         <div class="d-flex align-items-center justify-content-between gap-5 mb-4 pt-4">
           
           <h1 class="fw-bold fs-1">
@@ -30,7 +30,7 @@
         </div>
 
         <div class="mx-1">
-          <p class="fw-normal text-wrap">
+          <p class="fw-normal text-wrap detail-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur adipisci laboriosam temporibus delectus aliquam! Maiores hic earum porro consequuntur natus aliquam aut adipisci doloribus ut, deleniti accusantium molestias dolorum nobis tenetur sunt eveniet corrupti libero laudantium quidem ex nostrum eum enim. Architecto rerum voluptatum provident hic saepe recusandae fuga laboriosam odit quisquam, nihil possimus esse similique accusantium, porro sunt voluptatem corporis assumenda repellat, ea dolorem. Incidunt totam quaerat eveniet, quos nulla unde fuga sapiente dolore, cupiditate quam ex iure nesciunt officia delectus nihil iste error dolor eos, vel quidem eum dignissimos! Beatae ex reiciendis assumenda eaque alias voluptas saepe aliquam?
           </p>
         </div>
